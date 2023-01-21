7724c3987ab07ab7f2e92b1f709c24564f0b337f
Lauren <lauren.a.heavey@gmail.com>
2023-01-20 20:25:34 -0600

    Add reducers for remaining components, confirm feeding to review component.

--- src/components/SupportQuestion/SupportQuestion.jsx
+++ src/components/SupportQuestion/SupportQuestion.jsx
@@ -1,12 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function SupportQuestion () {
+    const dispatch = useDispatch();
+    const [supportInput, setSupportInput] = useState('');
+
+    const handleInput = () => {
+        console.log(supportInput);
+        dispatch({
+            type: 'SET_SUPPORT_ANSWER',
+            payload: supportInput
+        })
+    }
+
     return (
         <>
             <h2>How well are you being supported?</h2>
             <form>
-                <input type='number'></input>
-                <button>
+                <input 
+                type='number'
+                value={supportInput}
+                onChange={(event) => setSupportInput(event.target.value)}
+                ></input>
+                <button onClick={handleInput}>
                     <Link to='/CommentsQuestion'>
                     NEXT
                     </Link>
--- src/components/UnderstandingQuestion/UnderstandingQuestion.jsx
+++ src/components/UnderstandingQuestion/UnderstandingQuestion.jsx
@@ -1,12 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function UnderstandingQuestion () {
+    const dispatch = useDispatch();
+    const [understandingInput, setUnderstandingInput] = useState('');
+
+    const handleInput = () => {
+        console.log(understandingInput);
+        dispatch({
+            type: 'SET_UNDERSTANDING_ANSWER',
+            payload: understandingInput
+        })
+    }
+
     return (
         <>
             <h2>How well are you understanding the content?</h2>
             <form>
-                <input type='number'></input>
-                <button>
+                <input 
+                type='number'
+                value={understandingInput}
+                onChange={(event) => setUnderstandingInput(event.target.value)}
+                ></input>
+                <button onClick={handleInput}>
                     <Link to='/SupportQuestion'>
                     NEXT
                     </Link>
